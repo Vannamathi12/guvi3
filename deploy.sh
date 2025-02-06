78082bbac86cdc355ed9f82ff07dec68e9784c3d
Vannamathi <127740426+Vannamathi12@users.noreply.github.com>
2025-02-06 11:29:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u vannamathi124 -p mathi@123
-    docker tag test vannamathi124/docker_ex3
-    docker push vannamathi124/docker_ex3
+    docker tag test vannamathi124/final
+    docker push vannamathi124/final
     
